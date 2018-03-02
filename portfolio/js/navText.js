78f8749c0a8af5f5d456389a96a53b6da59d1508
country2 <hlc922@gmail.com>
2018-03-02 10:10:41 -0500

    added photo sto series

---
+++ portfolio/js/navText.js
@@ -0,0 +1,27 @@
+var nav = document.querySelector("nav");
+
+var navContainer = document.querySelector("#nav-container");
+
+
+
+
+function navText(){
+	
+	if (a.parentNode = navContainer){
+		echo "Look at me!";
+    } if else {
+		echo "Don't look at me.";
+    }
+    
+}
+    
+    
+    
+    
+var greet = function( username ){
+	if (username == null){
+		console.log("Hey there, what's your name?")
+    } else {
+		console.log("Hello " + username + ".");
+    }
+}
